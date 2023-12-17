f1331f4484a828f78ff57c5f335b6486f49298e9
Andrew <andrewbeaton123@gmail.com>
2023-12-17 02:20:29 +0000

    pre refactor  working multi thread and learning rate search

--- mc_6_simplified_multiprocess.py
+++ mc_6_simplified_multiprocess.py
@@ -1,11 +1,12 @@
 import numpy as np
 import logging 
 import  pickle as pkl
-logging.basicConfig(level="DEBUG")
+logging.basicConfig(level="INFO")
 import random
 import logging 
 import pickle as pkl
 # Monte Carlo Control Agent
+from tqdm import tqdm
 from itertools import product
 from typing import Dict
 from game.game_2 import TicTacToe
@@ -102,13 +103,38 @@ class SuperCarloAgent(MonteCarloAgent):
 
 
 def    mc_create_run_instance(args):
-        print("mc_create_run_instance - run")
-        episodes_in,all_states = args
-        agent= MonteCarloAgent(0.1,all_states)
+        #print("mc_create_run_instance - run")
+        episodes_in,all_states,lr = args
+        agent= MonteCarloAgent(lr,all_states)
         agent.initialize_q_values()
         agent.train(episodes_in)
-        print("mc_create_run_instance - finish")
+        #rint("mc_create_run_instance - finish")
         return episodes_in,agent.q_values
+def test_agent(args:(int ,SuperCarloAgent|MonteCarloAgent)) -> (int,int):
+    """_summary_
+
+    Args:
+        tests (_type_): _description_
+        int (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    tests,agent= args
+    wins , draws = 0 , 0
+    for _ in range(tests):
+        env = TicTacToe(random.choice([0,1]))
+        while not env.is_game_over():
+            if env.current_player == 1:
+                action = agent.epsilon_greedy_policy(env)
+            else:
+                action = np.random.choice(len(env.get_valid_moves()))
+            env.make_move(*env.get_valid_moves()[action])
+        if env.check_winner() == 1:
+            wins += 1
+        elif env.check_winner() ==0:
+            draws +=1
+    return wins,draws 
 
 def main():
         def generate_all_states():
@@ -132,75 +158,80 @@ def main():
         """
         overall_res = {}
         combined_q_values = {}
-        cores= 3
+        cores= 8
         last_e_total = 0
-        for episodes in range(100000,1000000,50000):#range(100000,1000000,100000):
-            logging.debug(f"main - Starting {episodes}")
-            
-            new_episodes  = episodes - last_e_total
-            core_episodes =   int(new_episodes/cores)
-            configs = [(core_episodes, all_possible_states) for _ in range(cores)]
-            logging.debug("main - Finished generating configs ")
-            #runs = create_run_mc(10)
-            logging.debug(f"main - cofig length is : {len(configs)}")
-            logging.debug(f"main - episodes configs are {[e_s[0] for e_s in configs]}")
-            if [e_s[0] for e_s in configs] != [0,0,0]:
-                start = time.process_time()
-                with Pool(cores) as pool: 
-                    
-                    logging.debug("main - within pool")
-                    results = pool.imap_unordered(mc_create_run_instance,configs)
-                    logging.debug("main - finished pool ")
-                    logging.debug(type(results))
-                    logging.debug(results._index)
-
-                    
-                    for runs, q_values in results:
-                        print("")
-                    print(f"Episodes time taken {time.process_time() - start}")
-                # Combine Q-values
-                
-                logging.debug("main- staring q vlaue combination")
-                
-                for episodes, q_values in results:
-                    logging.debug(f"main -q length {len((q_values).keys())}")
-                    for state_str, values in q_values.items():
-                        if state_str not in combined_q_values:
-                            combined_q_values[state_str] = np.array(values)
-                        else:
-                            combined_q_values[state_str] += np.array(values)
-                logging.debug("main- fished q vlaue combination")
-                last_e_total +=sum([e_s[0] for e_s in configs])
-            agent = SuperCarloAgent(combined_q_values,0.1)
-            wins = 0
-            draws= 0
-            logging.debug("main - starting test for combined q's")
-            for _ in range(10000):
-                env = TicTacToe(random.choice([0,1]))
-                while not env.is_game_over():
-                    if env.current_player == 1:
-                        action = agent.epsilon_greedy_policy(env)
-                    else:
-                        action = np.random.choice(len(env.get_valid_moves()))
-                    env.make_move(*env.get_valid_moves()[action])
-                if env.check_winner() == 1:
-                    wins += 1
-                elif env.check_winner() ==0:
-                    draws +=1
-
-            print(f"For Episodes : {episodes}")
-
-            print(f"Agent won {wins} out of 100000 games.")
-            print(f"Games drawn {draws}")
-            overall_res[episodes] = (wins,draws)
-            
-        with open("latest_overall_results.pkl", "wb") as f :
-            pkl.dump(overall_res,f)
         
-        with open("Combination_super_carlo.pkl","wb") as f2:
-            pkl.dump(agent, f2)
+        learning_rate = [0.1,0.01, 0.001]
+        for rate in tqdm(learning_rate, colour="green"):
+            
+            for episodes in tqdm(range(1,500000,50000)):#range(100000,1000000,100000):
+                
+                logging.debug(f"main - Starting {episodes}")
+                
+                new_episodes  = episodes - last_e_total
+                core_episodes =   int(new_episodes/cores)
+                configs = [(core_episodes, all_possible_states,rate) for _ in range(cores)]
+                logging.debug("main - Finished generating configs ")
+                #runs = create_run_mc(10)
+                logging.debug(f"main - cofig length is : {len(configs)}")
+                logging.debug(f"main - episodes configs are {[e_s[0] for e_s in configs]}")
+                if [e_s[0] for e_s in configs] != [0,0,0]:
+                    start = time.process_time()
+                    with Pool(cores) as pool: 
+                        
+                        logging.debug("main - within pool")
+                        results = pool.imap_unordered(mc_create_run_instance,configs)
+                        logging.debug("main - finished pool ")
+                        logging.debug(type(results))
+                        logging.debug(results._index)
 
-        return overall_res
+                        
+                        for runs, q_values in results:
+                            continue
+                        #print(f"Episodes time taken {time.process_time() - start}")
+                    # Combine Q-values
+                    
+                    logging.debug("main- staring q vlaue combination")
+                    
+                    for episodes, q_values in results:
+                        logging.debug(f"main -q length {len((q_values).keys())}")
+                        for state_str, values in q_values.items():
+                            if state_str not in combined_q_values:
+                                combined_q_values[state_str] = np.array(values)
+                            else:
+                                combined_q_values[state_str] += np.array(values)
+                    logging.debug("main- fished q vlaue combination")
+                    last_e_total +=sum([e_s[0] for e_s in configs])
+                agent_to_test = SuperCarloAgent(combined_q_values,0.1)
+                
+                tests_overall = 100000
+                test_count_pc= int(tests_overall/cores)
+                logging.debug("main - starting test for combined q's")
+                test_config = [(test_count_pc,agent_to_test) for _ in range(cores) ]
+                total_wins, total_draws= 0,0
+                with Pool(cores) as pool:
+                    res_test_games= pool.imap_unordered(test_agent,test_config)
+
+                    for wins_run, draw_run in res_test_games:
+                        total_wins += wins_run
+                        total_draws += draw_run
+                        #print("")
+                
+                
+
+                print(f"For Episodes : {episodes}")
+
+                print(f"Agent won {total_wins} out of {tests_overall} games.")
+                print(f"Games drawn {total_draws}")
+                overall_res[episodes] = (rate,total_wins,total_draws)
+                
+            with open("latest_overall_results.pkl", "wb") as f :
+                pkl.dump(overall_res,f)
+            
+            with open("Combination_super_carlo.pkl","wb") as f2:
+                pkl.dump(agent_to_test, f2)
+
+            return overall_res
 
 
 if __name__ == "__main__":
