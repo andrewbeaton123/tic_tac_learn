0f1a40cb28121928f691e0d4ebf4183953de66f0
Andrew Beaton <andrewbeaton123@gmail.com>
2026-04-11 15:57:32 +0100

    added version fetching for the trianign config

--- tic_tac_learn/agents/trained_model_abc.py
+++ tic_tac_learn/agents/trained_model_abc.py
@@ -1,14 +1,21 @@
 from abc import ABC
 from typing import Any, Dict
 from mlflow.models  import ModelSignature
+from importlib.metadata import version
+
 def TrainedModelABC(ABC):
 
     def __init__(self,
                  model_data : Any,
                  metadata: Dict,
-                 game_interface_type: str):
+                 game_interface_type: str,
+                 hyperparameters : Dict,
+                 training_config : Dict,
+                 meta_data: Dict ):
+        
         self._metadata = metadata
-
+        self._hyperparameters = hyperparameters
+        self._training_config = training_config
 
 
 
@@ -33,32 +40,35 @@ def TrainedModelABC(ABC):
     def load(self) -> None : 
         pass 
 
-    def to_dict() -> Dict : 
+    def to_dict(self) -> Dict : 
         pass
     
     @classmethod
     def from_dict() -> TrainedModelABC:
         pass 
 
-    def get_model_signature() -> ModelSignature:
+    def get_model_signature(self) -> ModelSignature:
         pass
 
-    def get_input_example() -> Any: 
+    def get_input_example(self) -> Any: 
         pass 
 
-    def get_model_flavor() -> str:
+    def get_model_flavor(self) -> str:
         # returns the way that the model should be treated
         return "pyfunc" 
 
-    def get_artifact_path() -> str: 
+    def get_artifact_path(self) -> str: 
         pass
     
-    def get_model_uri() -> str: 
+    def get_model_uri(self) -> str: 
         pass
 
-    def get_hyperparameters () -> Dict: 
-        pass
+    def get_hyperparameters (self) -> Dict: 
+        return self._hyperparameters
 
-    def get_training_config() -> Dict : 
-        pass 
-    
+    def get_training_config(self) -> Dict : 
+        version_config = {
+            "package_version": version("tic_tac_learn")
+        }
+        
+        return self._training_config | version_config
